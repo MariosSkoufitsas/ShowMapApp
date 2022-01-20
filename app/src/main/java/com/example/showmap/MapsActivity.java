a33b89c4700f7da3e45adec80aec2201d53988c4
MariosSkoufitsas <49339658+MariosSkoufitsas@users.noreply.github.com>
2022-01-20 13:47:55 +0200

    ShowApp

---
+++ app/src/main/java/com/example/showmap/MapsActivity.java
@@ -0,0 +1,50 @@
+package com.example.showmap;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private FirebaseFirestore db;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        db = FirebaseFirestore.getInstance();
+        db.collection("Marks").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for(QueryDocumentSnapshot documentSnapshot: queryDocumentSnapshots){
+                    Marker marker = documentSnapshot.toObject(Marker.class);
+                    LatLng latLng = new LatLng(marker.getLat(), marker.getLot());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(marker.getName()).snippet(marker.getSensor()).icon(BitmapDescriptorFactory.defaultMarker(Float.parseFloat(marker.getColor()))));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                }
+            }
+        });
+    }
+}
